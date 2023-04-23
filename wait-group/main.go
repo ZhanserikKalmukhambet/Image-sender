0ac2cda0c3274e5851518489daeb2061902436e4
Zhanserik <you@example.c git config --global user.name Your>
2023-04-23 23:37:29 +0600

    Creating a telegram-bot which sends random photos concurrently by using goroutines(wait-group)

---
+++ wait-group/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"encoding/json"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
+	"sync"
+)
+
+type UnsplashPhoto struct {
+	Urls struct {
+		Regular string `json:"regular"`
+	} `json:"urls"`
+}
+
+func GetRandomPhoto() (string, error) {
+	url := "https://api.unsplash.com/photos/random?client_id=" + os.Getenv("UNSPLASH_ACCESS_KEY") // random photo
+
+	resp, err := http.Get(url) // trying get request
+	if err != nil {
+		return "", err
+	}
+
+	defer resp.Body.Close() // will be closed at the end
+
+	var photo UnsplashPhoto // variable
+
+	err = json.NewDecoder(resp.Body).Decode(&photo) // store resp.Body to photo variable
+	if err != nil {
+		return "", err
+	}
+
+	return photo.Urls.Regular, nil
+}
+
+func main() {
+	err := godotenv.Load("../.env") // necessary for loading from .env
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("BOT_TOKEN"))
+	if err != nil {
+		panic(err)
+	}
+
+	bot.Debug = true // showing all actions, updates, etc.
+
+	log.Printf("Authorized on account %s", bot.Self.UserName) // if connection is correct, then it will print username
+
+	updateConfig := tgbotapi.NewUpdate(0) // update bot configuration each time
+	updateConfig.Timeout = 60
+
+	updates := bot.GetUpdatesChan(updateConfig) // getting updates which is stored in channels
+
+	count := 0
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		for update := range updates {
+			if update.Message == nil {
+				continue
+			}
+
+			log.Printf("[%s]%s", update.Message.From.UserName, update.Message.Text)
+
+			// check if user sends "/image" or "image" command
+			if update.Message.IsCommand() && update.Message.Command() == "image" || update.Message.Text == "image" {
+				count++
+				log.Printf("Count: %v", count)
+
+				photo, err := GetRandomPhoto()
+				if err != nil {
+					log.Println(err)
+				}
+
+				file := tgbotapi.NewPhoto(update.Message.Chat.ID, tgbotapi.FileURL(photo)) // instead of NewMessage
+
+				bot.Send(file)
+			}
+		}
+	}()
+
+	wg.Wait()
+}
